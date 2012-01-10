7d178290dfe563c3166ef5a58b1cef05ea8b990c
Pablo Chinea <pchinea@dpcat.es>
2012-01-10 13:36:43 +0000

    En el publicador se estaba importando una funciona que ya no existía, corregido.

--- dpcat/cb_publisher/forms.py
+++ dpcat/cb_publisher/forms.py
@@ -1,6 +1,5 @@
 #encoding: utf-8
 from django import forms
-from cb_publisher.functions import get_categories
 
 class ConfigForm(forms.Form):
     clipbucket_url = forms.CharField(label = u'URL del sitio Clipbucket')
