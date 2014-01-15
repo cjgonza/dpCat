2d352e512b2bb44596bd77d88ccf8932580bdee6
Pablo Chinea <pchinea@ull.edu.es>
2014-01-15 12:11:56 +0000

    Adaptadas las licencias Cretive Commons a su versión 4.0

--- clipbucket_plugin/dpcat/license_code.php
+++ clipbucket_plugin/dpcat/license_code.php
@@ -2,9 +2,9 @@
 
 $license_code = Array(
     'CR' => 'Todos los derechos reservados.',
-    'MD' => '<a target="_blank" href="http://creativecommons.org/licenses/by-nc/3.0/es/">Creative Commons: Reconocimiento - No Comercial.</a>',
-    'SA' => '<a target="_blank" href="http://creativecommons.org/licenses/by-nc-sa/3.0/es/">Creative Commons: Reconocimiento - No Comercial - Compartir Igual.</a>',
-    'ND' => '<a target="_blank" href="http://creativecommons.org/licenses/by-nc-nd/3.0/es/">Creative Commons: Reconocimiento - No Comercial - Sin Obra Derivada.</a>',
+    'MD' => '<a target="_blank" href="http://creativecommons.org/licenses/by-nc/4.0/deed.es_ES">Creative Commons: Reconocimiento - No Comercial.</a>',
+    'SA' => '<a target="_blank" href="http://creativecommons.org/licenses/by-nc-sa/4.0/deed.es_ES">Creative Commons: Reconocimiento - No Comercial - Compartir Igual.</a>',
+    'ND' => '<a target="_blank" href="http://creativecommons.org/licenses/by-nc-nd/4.0/deed.es_ES">Creative Commons: Reconocimiento - No Comercial - Sin Obra Derivada.</a>',
 );
 
 ?>
