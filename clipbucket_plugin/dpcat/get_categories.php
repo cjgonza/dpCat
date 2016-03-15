93b934779f86a06b8f9625fd90a0be05f1158a46
Moisés Lodeiro <moises.lodeiro@gmail.com>
2016-03-15 13:51:47 +0000

    Se ha cambiado la raíz del proyecto dpCat a un nivel superior
    La app de clipbucket se ha eliminado del repositorio. También se ha editado el
    settings para que por defecto tenga como configuración el sqlite3.

--- clipbucket_plugin/dpcat/get_categories.php
+++
@@ -1,6 +0,0 @@
-<?php
-require_once('../../includes/common.php');
-
-global $cbvid;
-echo json_encode($cbvid->get_categories());
-?>
--- clipbucket_plugin/dpcat/get_collection_categories.php
+++
@@ -1,6 +0,0 @@
-<?php
-require_once('../../includes/common.php');
-
-global $cbcollection;
-echo json_encode($cbcollection->get_categories());
-?>
